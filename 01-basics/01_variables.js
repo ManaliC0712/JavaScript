97ce71bcbf1494ec66c515f92f4d64d1c3bdaf1b
ManaliC0712 <108863379+ManaliC0712@users.noreply.github.com>
2024-01-03 09:15:50 +0000

    variables

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 112233
+let accountEmail = "manali.c@veerit.com"
+var accountPassword = "11111"
+accountCity = "Nagpur"
+let accountState;
+
+//accountId = 2
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState])
