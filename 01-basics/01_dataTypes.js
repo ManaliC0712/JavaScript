d98925464f98db63fc67cd5aeb43594985f9ea69
ManaliC0712 <108863379+ManaliC0712@users.noreply.github.com>
2024-01-03 09:15:18 +0000

    dataTypes

---
+++ 01-basics/01_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert("");
+
+let name = "Manali"
+let age = 24
+let isLoggedIn = false
+
+
+// number =>2 to power 52
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol 
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
