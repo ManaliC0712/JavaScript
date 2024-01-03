eb42bf503e37b9a6b2d0012ea932b7fc84a2649b
ManaliC0712 <108863379+ManaliC0712@users.noreply.github.com>
2024-01-03 10:43:59 +0000

    dataType-summary

---
+++ 01-basics/dataType_summary.js
@@ -0,0 +1,16 @@
+// Primitive dataTypes
+
+// 7 types: String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+
+const heros = ["manali", "mannu", "mona"];
+let myObj = {
+    name: "kittu",
+    age: 22,
+}
+
+const myFunction = function(){
+console.log("add text");
+}
+
+console.log(typeof heros);
