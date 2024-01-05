38cafed4f4c6a41d54aa261960da1ab3f2ccc876
ManaliC0712 <108863379+ManaliC0712@users.noreply.github.com>
2024-01-05 07:37:55 +0000

    Array_02_2

--- 02-basics/02-array.js
+++ 02-basics/02-array.js
@@ -1,10 +1,10 @@
 //array
 
-const myArr = [0, 1, 2, 3, 4, 5]
-const myHeros = ["kittu", "pooja"]
+// const myArr = [0, 1, 2, 3, 4, 5]
+// const myHeros = ["kittu", "pooja"]
 
-const myArr2 = new Array(1, 2, 3, 4)
-console.log(myArr[1]);
+// const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
 
 // myArr.push(6)
 // myArr.push(7)
@@ -12,19 +12,45 @@ console.log(myArr[1]);
 // myArr.unshift(9)
 // myArr.shift()
 
-console.log(myArr.includes(9));
-console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
 
 
-const newArr = myArr.join()
-console.log(myArr);
-console.log(typeof newArr);
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof newArr);
 
 //slice, splice
-console.log("A ", myArr);
-const myn1 = myArr.slice(1, 3)
-console.log(myn1)
-console.log("B ", myArr);
+// console.log("A ", myArr);
+// const myn1 = myArr.slice(1, 3)
+// console.log(myn1)
+// console.log("B ", myArr);
 
-const myn2 = myArr.splice(1, 3)
-console.log(myn2);
+// const myn2 = myArr.splice(1, 3)
+// console.log(myn2);
+
+
+const class_name = ["first", "second", "third", "fourth"]
+const subject = ["math", "english", "marathi", "hindi"]
+
+class_name.push(subject)
+// console.log(class_name);
+// console.log(class_name[3][1]);
+
+const allClass = class_name.concat(subject);
+// console.log(allClass)
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, [9, 10]]]
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array)
+
+
+console.log(Array.isArray("MANU"))
+console.log(Array.from("MANU"))
+console.log(Array.from({name: "MANU"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
