3481cf46fe393e1c782e5839fce382b61bd75e0f
ManaliC0712 <108863379+ManaliC0712@users.noreply.github.com>
2024-01-05 06:49:25 +0000

    dates

---
+++ 01-basics/dates.js
@@ -0,0 +1,28 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.getHours());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date("2023-02-12")
+console.log(myCreatedDate.toLocaleString());
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime())
+console.log(Date.now()/1000);
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
+console.log(newDate.getMonth() + 1);
+
+// `${newDate.getDay()}`
+
+
+newDate.toLocaleString("default", {
+    weekday: "long",
+})
+
