27b7b2d0354a569e3989406b43202c54bb457c06
ManaliC0712 <108863379+ManaliC0712@users.noreply.github.com>
2024-01-05 13:04:46 +0000

    02-objects_04

--- 02-basics/02-objects.js
+++ 02-basics/02-objects.js
@@ -35,3 +35,49 @@ JsUser.greeting2 = function(){
 }
 console.log(JsUser.greeting);
 console.log(JsUser.greeting2);
+
+
+//singleton
+
+// const tinderUser = new Object()
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "kittu"
+tinderUser.idLoggedIn = false
+
+const regularUser = {
+    email: "manali.c@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "kittu",
+            lastname: "kittu1234"
+        }
+    }
+}
+console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+const obj4 = Object.assign({}, obj1, obj2, obj3)
+console.log(obj4);
+
+// const users[
+//     {
+
+//     },
+//     {
+
+//     },
+//     {
+
+//     },
+//     {
+
+//     }
+// ]
+
+// users[1].email
+// console.log(tinderUser)
