4cabf314ea37838b6140b8e353afb6d5ffbe87aa
ManaliC0712 <108863379+ManaliC0712@users.noreply.github.com>
2024-01-08 10:27:28 +0000

    02-function-02

--- 02-basics/02-function.js
+++ 02-basics/02-function.js
@@ -12,3 +12,27 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 console.log(loginUserMessage("mannu"))
+
+
+function calculateCartPrice(val1, va2,...num1){
+return num1
+}
+console.log(calculateCartPrice(200, 300, 400, 500, 600))
+
+const user ={
+    username: "manu",
+    price: 777
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject({
+    username: "kittu",
+    price: 299
+})
+const myNewArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue([100, 200, 300 ,400]));
